7cec195ff5c5dfcac2eaf5f462e14786eb94b8b7
1prabhakarpal <115796639+1prabhakarpal@users.noreply.github.com>
2024-07-13 15:58:15 +0530

    added tailwind css

--- src/main/java/com/manager/contact/controller/PageController.java
+++ src/main/java/com/manager/contact/controller/PageController.java
@@ -1,14 +1,19 @@
 package com.manager.contact.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 
 @Controller
 public class PageController {
     
     @RequestMapping("/")
-    public String getHomePage() {
+    public String getHomePage(Model model) {
+        model.addAttribute("title", "Smart Contact Manager");
+        model.addAttribute("message", "Welcome to Smart Contact Manager!");
+        model.addAttribute("githubRepo", "https://github.com/1prabhakarpal/contact-manager");
         return "home";
     }
 
